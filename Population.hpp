106be8c13dd691c666affbfc0acc207adfc6a51b
ii2SA <148847319+ii2SA@users.noreply.github.com>
2023-12-06 14:42:35 +0100

    Update of Population.hpp and creation of methods

--- Population.hpp
+++ Population.hpp
@@ -1,1 +1,23 @@
+/* A = 00 , C = 01 , G = 10 , T = 11 */
+
+#include <iostream>
+#include <string>
+#include <ctime>
+
+class Population 
+{
+  private:
+    std::string _individu;
+    std::vector<std::string> _ListeDindividu;
+    int a;
+
+  public:
+    Population(std::string _individu, std::vector<std::string> _ListeDindividu);
+    ~Population();
+    Newindividu();
+    Addindividu();
+    MeilleureIndividu();
+    newpopulation();
+
+}
 
