70d04acdefa493852c65efc6cca376d52ffe6b68
ii2SA <148847319+ii2SA@users.noreply.github.com>
2023-12-21 19:35:21 +0100

    Update Population.cpp

--- Population.cpp
+++ Population.cpp
@@ -1,8 +1,38 @@
+#include <iostream>
+#include <random>
+#include <string>
 #include "Population.hpp"
 
-Population::Population(std::string _individu, std::vector<std::string> _ListeDindividu)
+Population::Population(std::string individu, std::vector<std::string> ListeDindividu)
 {
-  
+
+//alternative du prof concernant les lettres
+
+    std::string Individu(
+    "ACGT");
+    std::random_device rng;
+    std::uniform_int_distribution<> index_dist(0, Individu.size() - 1);
+    
+    std::string nucleotide;
+ 
+    for(int i = 0; i < 8; ++i) {
+     //   std::string nucleotide = Individu[index_dist(rng)].substr(3,2);
+     ;
+      //std::cout<<Individu[index_dist(rng)];
+      
+      nucleotide[i] = Individu[index_dist(rng)];
+      std::cout<<nucleotide[i];
+        
+}
+
+    // il ny a plus qua faire des pushback dans population
+   std::cout<<std::endl;
+
+    for(int i = 0; i < 8 ; ++i)
+    {
+     std::cout<<nucleotide[i];
+    }
+
 }
 
 Population::~Population()
